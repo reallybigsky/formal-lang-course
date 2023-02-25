4dede588758577353792f5a34739466ffddf1ef9
reallybigsky <do.not.focus@gmail.com>
2023-02-25 20:38:06 +0300

    hw1 tests rename

--- tests/test_graph_utils.py
+++ tests/test_graph_utils.py
@@ -3,8 +3,8 @@ import os
 from project import graph_utils
 
 
-def test_get_graphs_info_with_names_pr_and_ls():
-    def _inner_test_get_graphs_info_with_names_pr_and_ls(
+def test_get_graph_info_by_name():
+    def _inner_test_get_graph_info_by_name(
         name: str, nodes: int, edges: int, labels: set
     ):
         graph = graph_utils.get_graph_info_by_name(name)
@@ -12,11 +12,11 @@ def test_get_graphs_info_with_names_pr_and_ls():
         assert graph.edges == edges
         assert set(graph.labels_to_list()) == labels
 
-    _inner_test_get_graphs_info_with_names_pr_and_ls("pr", 815, 692, {"a", "d"})
-    _inner_test_get_graphs_info_with_names_pr_and_ls("ls", 1687, 1453, {"a", "d"})
+    _inner_test_get_graph_info_by_name("pr", 815, 692, {"a", "d"})
+    _inner_test_get_graph_info_by_name("ls", 1687, 1453, {"a", "d"})
 
 
-def test_create_and_save_two_cycles_graph():
+def test_create_and_save_two_cycles_graph_as_pydot():
     graph_utils.create_and_save_two_cycles_graph_as_pydot(
         42, 29, labels=("a", "d"), path="tmp_two_cycles_graph_42_29"
     )
