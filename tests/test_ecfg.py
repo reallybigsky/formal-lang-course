6eab4e248b06ce3cb314c3dbbbc3b623c2a871a0
reallybigsky <do.not.focus@gmail.com>
2023-04-08 02:48:29 +0300

    task_7 is done!

---
+++ tests/test_ecfg.py
@@ -0,0 +1,119 @@
+from tempfile import NamedTemporaryFile
+from textwrap import dedent
+
+from pyformlang.cfg import Variable
+from pyformlang.regular_expression import Regex
+
+from project.ecfg import ECFG
+
+
+def test_ecfg_from_pyformlang_cfg():
+    def check_ecfg_eq(text: str, expected: ECFG):
+        with NamedTemporaryFile(mode="w", delete=False) as f:
+            f.write(dedent(text))
+            path = f.name
+
+        actual = ECFG.from_file(path)
+
+        assert set(actual.productions.keys()) == set(expected.productions.keys())
+        for k in actual.productions.keys():
+            assert (actual.productions[k].to_epsilon_nfa().is_equivalent_to(expected.productions[k].to_epsilon_nfa()))
+        assert actual.start == expected.start
+
+    check_ecfg_eq(
+        """
+            S -> A B C
+            A -> a
+            B -> b
+            C -> c
+        """
+        ,
+        ECFG(
+            Variable("S"),
+            {
+                Variable("S"): Regex("A.B.C"),
+                Variable("A"): Regex("a"),
+                Variable("B"): Regex("b"),
+                Variable("C"): Regex("c")
+            }
+        )
+    )
+
+    check_ecfg_eq(
+        """
+            S -> a b c D
+            D -> E
+            E -> d
+        """
+        ,
+        ECFG(
+            Variable("S"),
+            {
+                Variable("S"): Regex("a.b.c.D"),
+                Variable("D"): Regex("E"),
+                Variable("E"): Regex("d")
+            }
+        )
+    )
+
+    check_ecfg_eq(
+        """
+            S -> A B
+            A -> a
+            B -> C
+            C -> c
+        """
+        ,
+        ECFG(
+            Variable("S"),
+            {
+                Variable("S"): Regex("A.B"),
+                Variable("A"): Regex("a"),
+                Variable("B"): Regex("C"),
+                Variable("C"): Regex("c")
+            }
+        )
+    )
+
+    check_ecfg_eq(
+        """
+            S -> A
+            A -> a
+            B -> b
+        """
+        ,
+        ECFG(
+            Variable("S"),
+            {
+                Variable("S"): Regex("A"),
+                Variable("A"): Regex("a"),
+                Variable("B"): Regex("b")
+            }
+        )
+    )
+
+    check_ecfg_eq(
+        """
+            S -> a b
+        """
+        ,
+        ECFG(
+            Variable("S"),
+            {
+                Variable("S"): Regex("a.b")
+            }
+        )
+    )
+
+    check_ecfg_eq(
+        """
+            S -> S S | a b | $
+        """
+        ,
+        ECFG(
+            Variable("S"),
+            {
+                Variable("S"): Regex("(S.S)|(a.b)|$")
+            }
+        )
+    )
---
+++ tests/test_recursive_automatons.py
@@ -0,0 +1,129 @@
+from pyformlang.cfg import Variable
+from pyformlang.regular_expression import Regex
+
+from project.ecfg import ECFG
+from project.recursive_automatons import RecursiveAutomata
+
+
+def test_recursive_automata():
+    def check_automata_eq(actual: RecursiveAutomata, expected: RecursiveAutomata):
+        assert set(actual.var_to_auto.keys()) == set(expected.var_to_auto.keys())
+        for k in actual.var_to_auto.keys():
+            assert actual.var_to_auto[k].is_equivalent_to(expected.var_to_auto[k])
+        assert actual.start == expected.start
+
+    check_automata_eq(
+        RecursiveAutomata.from_ecfg(
+            ECFG.from_text(
+                """
+                    S -> A B C
+                    A -> a
+                    B -> b
+                    C -> c
+                """
+            )
+        ),
+        RecursiveAutomata(
+            Variable("S"),
+            {
+                Variable("S"): Regex("A.B.C").to_epsilon_nfa(),
+                Variable("A"): Regex("a").to_epsilon_nfa(),
+                Variable("B"): Regex("b").to_epsilon_nfa(),
+                Variable("C"): Regex("c").to_epsilon_nfa()
+            }
+        )
+    )
+
+    check_automata_eq(
+        RecursiveAutomata.from_ecfg(
+            ECFG.from_text(
+                """
+                    S -> a b c D
+                    D -> E
+                    E -> d
+                """
+            )
+        ),
+        RecursiveAutomata(
+            Variable("S"),
+            {
+                Variable("S"): Regex("a.b.c.D").to_epsilon_nfa(),
+                Variable("D"): Regex("E").to_epsilon_nfa(),
+                Variable("E"): Regex("d").to_epsilon_nfa()
+            }
+        )
+    )
+
+    check_automata_eq(
+        RecursiveAutomata.from_ecfg(
+            ECFG.from_text(
+                """
+                    S -> A B
+                    A -> a
+                    B -> C
+                    C -> c
+                """
+            )
+        ),
+        RecursiveAutomata(
+            Variable("S"),
+            {
+                Variable("S"): Regex("A.B").to_epsilon_nfa(),
+                Variable("A"): Regex("a").to_epsilon_nfa(),
+                Variable("B"): Regex("C").to_epsilon_nfa(),
+                Variable("C"): Regex("c").to_epsilon_nfa()
+            }
+        )
+    )
+
+    check_automata_eq(
+        RecursiveAutomata.from_ecfg(
+            ECFG.from_text(
+                """
+                    S -> A
+                    A -> a
+                    B -> b
+                """
+            )
+        ),
+        RecursiveAutomata(
+            Variable("S"),
+            {
+                Variable("S"): Regex("A").to_epsilon_nfa(),
+                Variable("A"): Regex("a").to_epsilon_nfa(),
+                Variable("B"): Regex("b").to_epsilon_nfa()
+            }
+        )
+    )
+
+    check_automata_eq(
+        RecursiveAutomata.from_ecfg(
+            ECFG.from_text(
+                """
+                    S -> a b
+                """
+            )
+        ),
+        RecursiveAutomata(
+            Variable("S"),
+            {
+                Variable("S"): Regex("a.b").to_epsilon_nfa()
+            }
+        )
+    )
+
+    check_automata_eq(
+        RecursiveAutomata.from_ecfg(
+            ECFG.from_text(
+                """
+                    S -> S S | a b | $
+                """
+            )
+        ),
+        RecursiveAutomata(
+            Variable("S"),
+            {
+                Variable("S"): Regex("(a.b)|(S.S)|$").to_epsilon_nfa()
+            }
+        )
+    )
